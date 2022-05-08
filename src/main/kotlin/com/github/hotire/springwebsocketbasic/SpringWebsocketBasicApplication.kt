3bd876ea7e9a7aa4a9fdc89a2ee836204b5995f7
hotire <gngh0101@gmail.com>
2022-05-09 00:03:42 +0900

    init

---
+++ src/main/kotlin/com/github/hotire/springwebsocketbasic/SpringWebsocketBasicApplication.kt
@@ -0,0 +1,11 @@
+package com.github.hotire.springwebsocketbasic
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringWebsocketBasicApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringWebsocketBasicApplication>(*args)
+}
