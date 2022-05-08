29cb031164de3b842cc6f68933912f606469d8d8
hotire <gngh0101@gmail.com>
2022-05-09 00:09:22 +0900

    Add WebSocketConfig

---
+++ src/main/kotlin/com/github/hotire/springwebsocketbasic/WebSocketConfig.kt
@@ -0,0 +1,11 @@
+package com.github.hotire.springwebsocketbasic
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
+
+@Configuration
+@EnableWebSocketMessageBroker
+class WebSocketConfig : WebSocketMessageBrokerConfigurer {
+
+}
