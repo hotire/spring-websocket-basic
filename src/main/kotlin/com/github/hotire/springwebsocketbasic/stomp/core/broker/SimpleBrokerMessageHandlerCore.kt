68a946e2e47949caa8262a9bd705deca02c2bcb4
hotire <gngh0101@gmail.com>
2022-05-16 00:45:56 +0900

    Learn SimpleBrokerMessageHandlerCore

---
+++ src/main/kotlin/com/github/hotire/springwebsocketbasic/stomp/core/broker/SimpleBrokerMessageHandlerCore.kt
@@ -0,0 +1,6 @@
+package com.github.hotire.springwebsocketbasic.stomp.core.broker
+
+/**
+ * @see org.springframework.messaging.simp.broker.SimpleBrokerMessageHandler
+ */
+class SimpleBrokerMessageHandlerCore
